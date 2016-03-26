404f3f7a3993fae3a8da0518ff738e2f096a91a0
David Sherret <dsherret@gmail.com>
2016-03-26 19:20:48 -0400

    Upgrade to use typings.

--- dist/base-client.js
+++
@@ -1,7 +0,0 @@
-var BaseClient = (function () {
-    function BaseClient(baseUrl) {
-        this.baseUrl = baseUrl;
-    }
-    return BaseClient;
-})();
-exports.BaseClient = BaseClient;
--- server-bridge-base-client.d.ts
+++ server-bridge-base-client.d.ts
@@ -1,8 +1,6 @@
-declare module "server-bridge-base-client" {
-    export abstract class BaseClient {
-        protected baseUrl: string;
-        constructor(baseUrl: string);
-        protected abstract get<ReturnType>(url: string, args?: Object): Promise<ReturnType>;
-        protected abstract post<ReturnType>(url: string, sendObj: any, args?: Object): Promise<ReturnType>;
-    }
+export abstract class BaseClient {
+    protected baseUrl: string;
+    constructor(baseUrl: string);
+    protected abstract get<ReturnType>(url: string, args?: Object): Promise<ReturnType>;
+    protected abstract post<ReturnType>(url: string, sendObj: any, args?: Object): Promise<ReturnType>;
 }
